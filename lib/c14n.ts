556cd3b22a50eb132e66047d958ce548c854f7f6
Ivan Herman <ivan@ivan-herman.net>
2022-11-22 14:23:27 +0100

    Completed the simple case

--- lib/c14n.ts
+++ lib/c14n.ts
@@ -54,16 +54,21 @@ export class URDNA2015 {
     canonicalize(input_dataset: Graph): Graph {
         const retval: Graph = new Set();
 
-        // Step 2: 
+        // Step 2
+        // All quads are 'classified' depending on what bnodes they contain
+        // Results in a mapping from bnodes to all quads that they are part of.
         {
             for (const quad of input_dataset) {
                 const bnode_map = (t: rdf.Term): void => {
                     if (t.termType === "BlankNode") {
+                        // The value property does not return the '_:' part, whereas the
+                        // algorithm is defined in terms of the full blank node id...
                         const bnode: BNodeId = `_:${t.value}`;
                         if (this.state.bnode_to_quads[bnode] === undefined) {
-                            this.state.bnode_to_quads[bnode] = [];
+                            this.state.bnode_to_quads[bnode] = [quad];
+                        } else {
+                            this.state.bnode_to_quads[bnode].push(quad);
                         }
-                        this.state.bnode_to_quads[bnode].push(quad);
                     }
                 }
                 bnode_map(quad.subject);
@@ -74,25 +79,108 @@ export class URDNA2015 {
         }
 
         // Step 3
-        const non_normalized_ids = Object.keys(this.state.bnode_to_quads);
+        // Collect the list of bnodes in a separate list for easier reference...
+        // This will store the list of bnodes that have not yet received a canonical id.
+        // Obviously, it initializes to the full list of bnodes in the input
+        let non_normalized_ids: BNodeId[] = Object.keys(this.state.bnode_to_quads);
 
         // Step 4
-        let simple: boolean = true;
+        // --- in FPWD: let simple: boolean = true;
 
         // Step 5
-        while(simple) {
-            simple = false;
+        // --- in FPWD: while(simple) {
+        {
+            // Step 5.1
+            // --- in FPWD: simple = false;
+
+            // Step 5.2 
+            this.state.hash_to_bnodes = {};
+
+            // Step 5.3
+            // Compute a hash value for each bnode (depending on the quads it appear in)
+            // In simple cases a hash value refers to one bnode only; in unlucky cases there
+            // may be more. Hence the usage of the hash_to_bnodes map.
             non_normalized_ids.forEach((n: BNodeId): void => {
-                this.compute_first_degree_hashes(n)
-                // ITT TARTOK!!!!
+                // Step 5.3.1
+                const hfn: Hash = this.compute_first_degree_hashes(n)
+                // Step 5.3.2
+                if (this.state.hash_to_bnodes[hfn] === undefined) {
+                    this.state.hash_to_bnodes[hfn] = [n];
+                } else {
+                    this.state.hash_to_bnodes[hfn].push(n);
+                }
             });
 
+            // Step 5.4
+            // For each hash take the corresponding bnode, and issue a new, canonical id in a sequence.
+            // This works only for those hashes where there is one associated bnode; for the ones
+            // where this is not the case, step 6 will kick in later.
+            // It is important to order the hashes, because the order of issuing the canonical id is
+            // important.
+            const hashes: Hash[] = Object.keys(this.state.hash_to_bnodes).sort();
+            for (const hash of hashes) {
+                const identifier_list: BNodeId[] = this.state.hash_to_bnodes[hash];
+                // Step 5.4.1
+                // Filter out the nasty cases
+                if (identifier_list.length > 1) continue;
 
+                // Step 5.4.2
+                // Here is the essential part: issue the canonical identifier.
+                // Note that the IdIssuer automatically stores the (existing, issued) pairs for the
+                // bnode identifier.
+                const existing_id = identifier_list[0];
+                const issued_id: BNodeId = this.state.canonical_issuer.issue_id(existing_id);
+
+                // Step 5.4.3
+                // Remove the bnode from the list of those that still need to be normalized
+                //
+                // Not sure this is the most efficient thing to do, but I go
+                // for clarity rather than efficiency at this point.
+                non_normalized_ids = non_normalized_ids.filter((key: BNodeId): boolean => key !== existing_id);
+
+                // Step 5.4.4
+                // Also remove the corresponding hash
+                delete this.state.hash_to_bnodes[hash];
+
+                // Step 5.4.5
+                // --- in FPWD: simple = true;
+            }
         }
 
+        console.log(this.state.canonical_issuer.issued_id_list);
 
+        // Step 6
+        {
+            const hashes: Hash[] = Object.keys(this.state.hash_to_bnodes).sort();
+            if (hashes.length > 0) {
+                console.log("This is the complex case that should not occur at this point yet!!!")
+            }
+        }
+
+        // Step 7
+        {
+            // This function replaces the term with its canonical equivalent, if applicable
+            const replace_bnode = (term: rdf.Term): rdf.Term => {
+                if (term.termType === "BlankNode") {
+                    const canonical = this.state.canonical_issuer.map_to_canonical(`_:${term.value}`);
+                    console.log(canonical)
+                    // Remove the `_:` before creating the new bnode...
+                    return DataFactory.blankNode(canonical.slice(2))
+                } else {
+                    return term;
+                }
+            };
+            for (const quad of input_dataset) {
+                // Step 7.1 & 7.2
+                const subject_copy = replace_bnode(quad.subject) as rdf.Quad_Subject;
+                const object_copy  = replace_bnode(quad.object) as rdf.Quad_Object;
+                const graph_copy   = replace_bnode(quad.graph) as rdf.Quad_Graph;
+                retval.add(DataFactory.quad(subject_copy,quad.predicate,object_copy,graph_copy))
+            }
+        }
         return retval;
     }
+
     debug() {
         console.log(JSON.stringify(this.state,null,4));
     }
--- lib/common.ts
+++ lib/common.ts
@@ -28,27 +28,36 @@ interface IdListItem {
     issued   : BNodeId;
 }
 
-const issued_id_list: IdListItem[] = [];
-
 export class IdIssuer {
-    prefix: string;
-    counter: number;
+    private _prefix: string;
+    private _counter: number;
+    private _issued_id_list: IdListItem[];
 
     constructor() {
-        this.prefix  = Constants.BNODE_PREFIX;
-        this.counter = 0;
+        this._prefix         = Constants.BNODE_PREFIX;
+        this._counter        = 0;
+        this._issued_id_list = [];
     }
 
     issue_id(existing: BNodeId): BNodeId {
-        const list_item = issued_id_list.find((item: IdListItem): boolean => item.existing === existing)
+        const list_item = this._issued_id_list.find((item: IdListItem): boolean => item.existing === existing)
         if (list_item !== undefined) {
             return list_item.issued
         } else {
-            const issued: BNodeId = `${this.prefix}${this.counter}`;
-            issued_id_list.push({existing, issued});
-            this.counter++;
+            const issued: BNodeId = `${this._prefix}${this._counter}`;
+            this._issued_id_list.push({existing, issued});
+            this._counter++;
             return issued;
         }
+    }
+
+    map_to_canonical(existing: BNodeId): BNodeId {
+        const item: IdListItem = this._issued_id_list.find((value: IdListItem): boolean => value.existing === existing)
+        return (item) ? item.issued : existing;
+    }
+
+    get issued_id_list() : IdListItem[] {
+        return this._issued_id_list;
     }
 }
 
